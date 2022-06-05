10e05f3895e0e9bd7d1cebd68ee10c10349930a8
karthi <karthirajendran12003@gmail.com>
2022-06-05 19:45:18 +0530

    ui

--- index.js
+++ index.js
@@ -27,6 +27,17 @@ app.get('/branch',(req,res)=>{+
   
 })
 
+app.get('/branch/view',(req,res)=>{
+    let sql = `SELECT * FROM BRANCH`
+    connection.query(sql,(err,rows,fields)=>{
+        if (err) console.log(err.message)
+        else{
+           res.render('branchView.ejs',{rows})
+        }
+    })
+
+})
+
 app.post('/branch',(req,res)=>{
     let {Branch_Name,Location}=req.body
     const Branch_Id=Math.floor(1000* Math.random()*9000)
@@ -64,10 +75,24 @@ app.delete('/branch',(req,res)=>{
     })
 })
 
+//BOOKS
+
 app.get('/book',(req,res)=>{
     
     res.render('book.ejs')
 })
+
+app.get('/book/view',(req,res)=>{  
+    var sql = `SELECT * FROM BOOK`
+    connection.query(sql,(err,rows,fields)=>{
+        if (err) console.log(err.message)
+        else{
+            
+           res.render('bookView.ejs',{rows})
+        }
+    })
+})
+
 
 app.post('/book',(req,res)=>{
     let {Book_Name,Book_Author,Isbn,Book_Edition} =req.body.books;
@@ -108,25 +133,16 @@ app.delete('/book',(req,res)=>{
     })  
 })
 
-app.get('/view/books',(req,res)=>{  
-    var sql = `SELECT * FROM BOOKS`
-    connection.query(sql,(err,rows,fields)=>{
-        if (err) console.log(err.message)
-        else{
-            console.log(rows)
-            res.redirect('/')
-        }
-    })
-})
 
 
+//---------------
 
 app.get('/return',(req,res)=>{
     res.render('return.ejs')
 })
 app.post('/return',(req,res)=>{
     const {Book_Id,Issue_Id,Staff_Id,Member_ID,Book_Name,Issue_Date,Expiry_Date}=req.body;
-    const Return_Id = uuidv4();
+    const Return_Id = Math.floor(1000* Math.random()*9000)
     var sql= `INSERT INTO RETURN(Book_Id,Issue_Id,Staff_Id,Member_Id,Book_Name,Issue_Date,Expiry_Date,Return_ID) VALUES("${Book_Id}",
     "${Issue_Id}","${Staff_Id}","${Member_ID}","${Book_Name}","${Issue_Date}","${Expiry_Date}","${Return_Id}")`
     connection.query(sql,(err,rows,fields)=>{
@@ -137,13 +153,24 @@ app.post('/return',(req,res)=>{
         }
     })
 })
+
 app.get('/member',(req,res)=>{
     res.render('member.ejs')
 })
 
+app.get("/member/view",(req,res)=>{
+    let sql = `SELECT * FROM MEMBER NATURAL JOIN BRANCH`
+    connection.query(sql,(err,rows,fields)=>{
+        if (err) console.log(err.message)
+        else{
+            res.render("memberView.ejs",{rows});
+        }
+    })
+})
+
 app.post('/member',(req,res)=>{
     let {Member_Name,Member_Email,Member_Phone,Member_Address,Branch_Id}=req.body
-    const MEMBER_ID=uuidv4();
+    const MEMBER_ID=Math.floor(1000* Math.random()*9000)
     var sql = `INSERT INTO MEMBER(MEMBER_NAME,MEMBER_ID,MEMBER_EMAIL,MEMBER_PHONE,MEMBER_ADDRESS,BRANCH_ID) VALUES("${Member_Name}","${MEMBER_ID}","${Member_Email}","${Member_Phone}","${Member_Address}","${Branch_Id}");`
     connection.query(sql,(err,rows,fields)=>{
         if (err) console.log(err.message)
@@ -180,10 +207,24 @@ app.delete('/member',(req,res)=>{
 app.get('/issue',(req,res)=>{
     res.render('issue.ejs')
 })
+
+app.get('/issue/view',(req,res)=>{
+
+
+    let sql =`SELECT * FROM ISSUE,BOOK,Member,BRANCH WHERE ISSUE.Book_Id = BOOK.Book_Id and MEMBER.Member_Id = ISSUE.Member_Id and MEMBER.Branch_Id=BRANCH.Branch_Id;`
+    connection.query(sql,(err,rows,fields)=>{
+        if (err) console.log(err.message)
+        else{
+            res.render("issueView",{rows})
+        }
+    })
+
+})
 app.post("/issue",(req,res)=>{
-    const {Book_Id,Book_Name,Issue_Date,Expiry_Date,Member_ID}= req.body
-    const Issue_Id = uuidv4();
-    var sql =`INSERT INTO ISSUE(Issue_Id,Book_Id,Book_Name,Date_Issue,Date,Expiry,Member_Id) VALUES("${Issue_Id}","${Book_Id}","${Book_Name}","${Issue_Date}","${Expiry_Date}","${Member_ID}")`
+    const {Book_Id,Issue_Date,Return_Date,Member_Id}= req.body
+    const Issue_Id = Math.floor(1000* Math.random()*9000)
+    var sql =`INSERT INTO ISSUE(Issue_Id,Book_Id,Date_Issue,Date_Expiry,Member_Id) VALUES("${Issue_Id}","${Book_Id}","${Issue_Date}",
+    "${Return_Date}","${Member_Id}")`
     connection.query(sql,(err,rows,fields)=>{
         if (err) console.log(err.message)
         else{
@@ -226,6 +267,17 @@ app.get('/staff',(req,res)=>{
     res.render('staff.ejs')
 })
 
+
+app.get('/staff/view',(req,res)=>{
+    var sql =`SELECT * FROM STAFF NATURAL JOIN BRANCH`
+    connection.query(sql,(err,rows,fields)=>{
+        if (err) console.log(err.message)
+        else{
+            console.log(rows);
+            res.render("staffView.ejs",{rows});
+        }
+    })
+})
 app.post('/staff',(req,res)=>{
     let {Staff_Name,Staff_Address,Staff_Gender,Staff_Phone,Branch_Id} = req.body
     const Staff_Id=Math.floor(1000* Math.random()*9000)
@@ -265,23 +317,6 @@ app.delete('/staff',(req,res)=>{
     })
 })
 
-app.get('/return',(req,res)=>{
-    res.render('return.ejs')
-})
-
-app.post('/return',(req,res)=>{
-    let {Staff_Name,Staff_Address,Staff_Gender,Staff_Phone,Branch_Id} = req.body
-    const Staff_Id=Math.floor(1000* Math.random()*9000)
-    var sql=`INSERT INTO STAFF(Staff_id,Staff_Name,Staff_Address,Staff_Gender,Staff_Phone,Branch_Id) VALUES("${Staff_Id}","${Staff_Name}","${Staff_Address}",
-    "${Staff_Gender}","${Staff_Phone}","${Branch_Id}");`
-    connection.query(sql,(err,rows,fields)=>{
-        if (err) console.log(err.message)
-        else{
-            console.log(rows);
-            res.redirect("/");
-        }
-    })
-})
 
 app.listen(5000,()=>{
     console.log("Server is running")
